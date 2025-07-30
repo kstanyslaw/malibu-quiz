2700e9477415c666a7714d2d461cd073d81120bd
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2025-07-31 00:25:07 +0500

    (feat)[answers] add answers service

---
+++ src/app/common/services/answers.service.ts
@@ -0,0 +1,20 @@
+import { inject, Injectable } from '@angular/core';
+import { addDoc, collection, CollectionReference, Firestore } from '@angular/fire/firestore';
+import { UserAnswers } from '@common/interfaces';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnswersService {
+  private readonly firestore: Firestore = inject(Firestore);
+  collection: CollectionReference ;
+
+  constructor() {
+    this.collection = collection(this.firestore, 'answers');
+  }
+
+  async addUserAnswers(userAnswers: UserAnswers) {
+    const newUserAnswers = await addDoc(this.collection, <UserAnswers> userAnswers);
+    return newUserAnswers;
+  }
+}
--- src/app/common/services/index.ts
+++ src/app/common/services/index.ts
@@ -1,2 +1,3 @@
 export * from './auth.service';
 export * from './question.service';
+export * from './answers.service';
