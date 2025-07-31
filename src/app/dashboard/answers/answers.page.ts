ec372fcd92aa00f0fd41aa47b10c9d8ae501851c
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2025-07-31 14:38:58 +0500

    (feat)[dashboard] add tabs

---
+++ src/app/dashboard/answers/answers.page.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-answers',
+  templateUrl: './answers.page.html',
+  styleUrls: ['./answers.page.scss'],
+  standalone: false,
+})
+export class AnswersPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
