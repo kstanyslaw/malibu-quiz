0e696a17f53b79196b963f2ff9d1f05e4398241c
Stanislav Kositsyn <k.stanyslaw@gmail.com>
2025-07-31 19:00:16 +0500

    (feat)[home] add checkboxes answeres

--- src/app/home/question-card/question-card.component.ts
+++ src/app/home/question-card/question-card.component.ts
@@ -13,9 +13,20 @@ export class QuestionCardComponent {
   @Input() answer!: string;
   @Output() answerChange = new EventEmitter<string>();
 
+  selectedOptions: string[] = [];
+
   constructor() {}
 
   onAnswerChange() {
     this.answerChange.emit(this.answer);
+  }
+
+  onCheckboxChange(option: string, isChecked: boolean) {
+    if (isChecked) {
+      this.selectedOptions.push(option);
+    } else {
+      this.selectedOptions = this.selectedOptions.filter(item => item !== option);
+    }
+    this.answerChange.emit(this.selectedOptions.toString());
   }
 }
